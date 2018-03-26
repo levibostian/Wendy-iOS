2b4d9c9877c9f8deb0e95862bec1635a0262ce66
Levi Bostian <levi.bostian@gmail.com>
2018-03-26 12:29:46 -0500

    Create new library from pod lib create.

---
+++ Example/Pods/Target Support Files/Pods-Wendy-iOS_Example/Pods-Wendy-iOS_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wendy_iOS_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Wendy_iOS_Example
+@end
---
+++ Example/Pods/Target Support Files/Wendy-iOS/Wendy-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Wendy_iOS : NSObject
+@end
+@implementation PodsDummy_Wendy_iOS
+@end
