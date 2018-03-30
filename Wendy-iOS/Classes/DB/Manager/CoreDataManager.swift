24f04853a86aaa87628f5af4d12231c89a87dad5
Levi Bostian <levi.bostian@gmail.com>
2018-03-30 15:10:14 -0500

    Bring back compatibility with ios version 8.0 for pod.

--- Wendy-iOS/Classes/DB/Manager/CoreDataManager.swift
+++ Wendy-iOS/Classes/DB/Manager/CoreDataManager.swift
@@ -16,20 +16,47 @@
     }
 
     internal lazy var viewContext: NSManagedObjectContext = {
-        return persistentContainer.viewContext
+        var managedObjectContext: NSManagedObjectContext?
+        if #available(iOS 10.0, *) {
+            managedObjectContext = self.persistentContainer.viewContext
+        } else {
+            // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+            let coordinator = self.persistentStoreCoordinator
+            managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+            managedObjectContext?.persistentStoreCoordinator = coordinator
+        }
+        return managedObjectContext!
     }()
 
-    lazy var persistentContainer: NSPersistentContainer = {
+    private lazy var applicationDocumentsDirectory: URL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named in the application's documents Application Support directory.
+        var documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+        documentUrl.appendPathComponent("WendyDataModel.sqlite")
+        return documentUrl
+    }()
+
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelURL = Bundle.bundleUrlForWendyFramework().url(forResource: "Wendy", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        try! coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: self.applicationDocumentsDirectory, options: nil)
+        return coordinator
+    }()
+
+    @available(iOS 10.0, *)
+    private lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let modelURL = Bundle.bundleUrlForWendyFramework().url(forResource: "Wendy", withExtension: "momd")!
-        let model = NSManagedObjectModel(contentsOf: modelURL)!
-        let container = NSPersistentContainer(name: "Wendy", managedObjectModel: model)
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        let container = NSPersistentContainer(name: "Wendy", managedObjectModel: self.managedObjectModel)
+        container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -42,107 +69,22 @@
                  * The store could not be migrated to the current model version.
                  Check the error message to determine what the actual problem was.
                  */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                fatalError("Unresolved error with core data \(error), \(error.userInfo)")
             }
         })
         return container
     }()
 
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
+    internal func saveContext () {
+        if self.viewContext.hasChanges {
             do {
-                try context.save()
+                try self.viewContext.save()
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
     }
-
-//    internal lazy var viewContext: NSManagedObjectContext = {
-//        var managedObjectContext: NSManagedObjectContext?
-//        if #available(iOS 10.0, *) {
-//            managedObjectContext = self.persistentContainer.viewContext
-//        } else {
-//            // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-//            let coordinator = self.persistentStoreCoordinator
-//            managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-//            managedObjectContext?.persistentStoreCoordinator = coordinator
-//        }
-//        return managedObjectContext!
-//    }()
-//
-//    private lazy var applicationDocumentsDirectory: URL = {
-//        // The directory the application uses to store the Core Data store file. This code uses a directory named in the application's documents Application Support directory.
-//        var documentUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
-//        documentUrl.appendPathComponent("WendyDataModel.sqlite")
-//        return documentUrl
-//    }()
-//
-//    private lazy var managedObjectModel: NSManagedObjectModel = {
-//        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-////        let modelURL = Bundle.main.url(forResource: "Wendy", withExtension: "momd")!
-//        let modelURL = Bundle.urlForWendyFramework().url(forResource: "Wendy", withExtension: "momd")!
-//        return NSManagedObjectModel(contentsOf: modelURL)!
-//
-////
-////        //    NSURL *modelURL = [[NSBundle bundleWithIdentifier:@"kemmitorz.myFramework"] URLForResource:@"Database" withExtension:@"momd"];
-////        Bundle.allFrameworks["Wendy"]
-////        guard let bundleURL = NSBundle(forClass: FrameworkClass.self).URLForResource("myFramework", withExtension: "bundle") else { throw Error }
-////        guard let frameworkBundle = NSBundle(URL: bundleURL) else { throw Error }
-////        guard let momURL = frameworkBundle.URLForResource("Database", withExtension: "momd") else { throw Error }
-//
-//    }()
-//
-//    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-//        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-//        try! coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: self.applicationDocumentsDirectory, options: nil)
-//        return coordinator
-//    }()
-//
-//    @available(iOS 10.0, *)
-//    private lazy var persistentContainer: NSPersistentContainer = {
-//        /*
-//         The persistent container for the application. This implementation
-//         creates and returns a container, having loaded the store for the
-//         application to it. This property is optional since there are legitimate
-//         error conditions that could cause the creation of the store to fail.
-//         */
-//        let container = NSPersistentContainer(name: "Wendy")
-//        container.loadPersistentStores(completionHandler: { (_, error) in
-//            if let error = error as NSError? {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//
-//                /*
-//                 Typical reasons for an error here include:
-//                 * The parent directory does not exist, cannot be created, or disallows writing.
-//                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-//                 * The device is out of space.
-//                 * The store could not be migrated to the current model version.
-//                 Check the error message to determine what the actual problem was.
-//                 */
-//                fatalError("Unresolved error with core data \(error), \(error.userInfo)")
-//            }
-//        })
-//        return container
-//    }()
-//
-//    internal func saveContext () {
-//        if self.viewContext.hasChanges {
-//            do {
-//                try self.viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nserror = error as NSError
-//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//            }
-//        }
-//    }
  }
