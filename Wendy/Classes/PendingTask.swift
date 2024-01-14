c601b2d98dbab07c0847d5b0fca86de3e18abdd6
Levi Bostian <levi@levi.earth>
2024-01-14 07:02:19 -0600

    refactor: remove PendingTasksFactory
    We are changing the way that tasks are executed by Wendy. 1 function will be called in a user's app to run tasks. The factory no longer adds value if we are going to call this 1 function.
    
    commit-id:5227ca7e

--- Wendy/Classes/PendingTask.swift
+++ Wendy/Classes/PendingTask.swift
@@ -11,6 +11,10 @@ public struct PendingTask {
         return PendingTask(tag: tag, taskId: nil, dataId: dataId, groupId: groupId, createdAt: nil)
     }
     
+    internal static func persisted(_ persistedPendingTask: PersistedPendingTask) -> PendingTask {
+        return PendingTask(tag: persistedPendingTask.tag!, taskId: persistedPendingTask.id, dataId: persistedPendingTask.dataId, groupId: persistedPendingTask.groupId, createdAt: persistedPendingTask.createdAt)
+    }
+    
     internal func from(persistedPendingTask: PersistedPendingTask) -> PendingTask {
         return PendingTask(tag: self.tag, taskId: persistedPendingTask.id, dataId: self.dataId, groupId: self.groupId, createdAt: persistedPendingTask.createdAt)
     }
