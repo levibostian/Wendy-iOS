5f9a8e02c4dec6a4e8827dc522f565636fcd3e69
Levi Bostian <levi@levi.earth>
2024-02-09 18:45:27 -0600

    refactor: delete all coredata code from the codebase
    As part of migration away from coredata, this is a cleanup step. This deletes all of the coredata code from the codebase and removes the coredata framework from the cocoapod dependency spec.
    
    This change now allows us to more easily support Wendy as a swift package for swift package manager!
    
    commit-id:22186d01

--- Wendy/Classes/PendingTask.swift
+++ Wendy/Classes/PendingTask.swift
@@ -6,10 +6,6 @@ public struct PendingTask: Codable {
     public let dataId: String?
     public let groupId: String?
     public let createdAt: Date? // populated later
-    
-    internal static func from(persistedPendingTask: PersistedPendingTask) -> PendingTask {
-        return PendingTask(tag: persistedPendingTask.tag!, taskId: persistedPendingTask.id, dataId: persistedPendingTask.dataId, groupId: persistedPendingTask.groupId, createdAt: persistedPendingTask.createdAt)
-    }
         
     public init(tag: String, taskId: Double?, dataId: String?, groupId: String?, createdAt: Date?) {
         self.tag = tag
