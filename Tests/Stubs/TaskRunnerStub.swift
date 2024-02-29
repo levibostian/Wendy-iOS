fb5de7064362042fa9e65f0264af019b6ff3baf6
Levi Bostian <levi@levi.earth>
2024-02-29 07:03:29 -0600

    refactor: move from GCD to swift concurrency for internal running code
    To make the code easier to read and maintain in the future, this change goes away from iOS GCD to swift concurrency. This has reduced the lines of code as well as made the code much easier to read. No public facing API changes done here.
    
    commit-id:03a95e99

--- Tests/Stubs/TaskRunnerStub.swift
+++ Tests/Stubs/TaskRunnerStub.swift
@@ -11,8 +11,14 @@ import Foundation
 class TaskRunnerStub: WendyTaskRunner {
     
     @Atomic var resultsQueue: [Result<Void?, Error>] = []
+    var runTaskClosure: ((String, String?, @escaping (Error?) -> Void) -> Void)? = nil
     
     func runTask(tag: String, dataId: String?, complete: @escaping (Error?) -> Void) {
+        if let runTaskClosure {
+            runTaskClosure(tag, dataId, complete)
+            return
+        }
+        
         let result = resultsQueue.removeFirst()
         
         switch result {
