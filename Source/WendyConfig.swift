9cd829060a92d46a6f987ace879f4acfb839963a
Levi Bostian <levi@levi.earth>
2024-03-20 06:36:16 -0500

    refactor: enable complete concurrency checking and fix warnings
    Enable complete swift concurrency compiler warnings and then resolve all of the warnings.
    
    This should be a backwards compatible refactor.
    
    commit-id:d77f0502

--- Source/WendyConfig.swift
+++ Source/WendyConfig.swift
@@ -1,80 +1,69 @@
 import Foundation
 
-public class WendyConfig {
-    public static var logTag: String = "WENDY"
-    public static var automaticallyRunTasks: Bool = true
-    public static var strict: Bool = true
-    public static var debug: Bool = false
-
-    fileprivate static var taskRunnerListeners: [WeakReferenceTaskRunnerListener] = []
-    public class func addTaskRunnerListener(_ listener: TaskRunnerListener) {
-        taskRunnerListeners.append(WeakReferenceTaskRunnerListener(listener: listener))
+public final class WendyConfig: Sendable {
+    let _logTag = MutableSendable("WENDY")
+    let _strict = MutableSendable(true)
+    let _debug = MutableSendable(false)
+    let _automaticallyRunTasks = MutableSendable(true)
+    
+    internal var taskRunnerListeners: [WeakReferenceTaskRunnerListener] {
+        get { _taskRunnerListeners.get() }
+        set { _taskRunnerListeners.set(newValue) }
     }
+    let _taskRunnerListeners = MutableSendable([WeakReferenceTaskRunnerListener]())
 
-    private static var taskStatusListeners: [TaskStatusListener] = []
-    public class func addTaskStatusListenerForTask(_ taskId: Double, listener: PendingTaskStatusListener) {
-        taskStatusListeners.append(TaskStatusListener(taskId: taskId, weakRefListener: WeakReferencePendingTaskStatusListener(listener: listener)))
-
-        // The task runner could be running this task right now and because it takes a while potentially to run a task, I need to notify the listener here. This should be the only use case to handle here, running of a task.
-        let taskRunner: PendingTasksRunner = DIGraph.shared.pendingTasksRunner
-        if taskRunner.currentlyRunningTask?.taskId == taskId {
-            listener.running(taskId: taskId)
-        }
+    internal var taskStatusListeners: [TaskStatusListener] {
+        get { _taskStatusListeners.get() }
+        set { _taskStatusListeners.set(newValue) }
     }
+    let _taskStatusListeners = MutableSendable([TaskStatusListener]())
 
     internal class func getTaskStatusListenerForTask(_ taskId: Double) -> [WeakReferencePendingTaskStatusListener] {
-        return taskStatusListeners.filter { (listener) -> Bool in
+        return Wendy.config.taskStatusListeners.filter { (listener) -> Bool in
             listener.taskId == taskId
         }.map { (taskStatusListener) -> WeakReferencePendingTaskStatusListener in
             taskStatusListener.weakRefListener
         }
     }
 
-    private struct TaskStatusListener {
+    internal struct TaskStatusListener {
         let taskId: Double
         let weakRefListener: WeakReferencePendingTaskStatusListener
     }
 }
 
-internal extension WendyConfig {
-    class func logNewTaskAdded(_ task: PendingTask) {
-        WendyConfig.taskRunnerListeners.forEach { weakRefListener in
-            weakRefListener.listener?.newTaskAdded(task)
+// Public API to modify WendyConfig
+public extension WendyConfig {
+    static var logTag: String {
+        get { Wendy.config._logTag.get() }
+        set { Wendy.config._logTag.set(newValue) }
+    }
+    
+    static var strict: Bool {
+        get { Wendy.config._strict.get() }
+        set { Wendy.config._strict.set(newValue) }
+    }
+    
+    static var debug: Bool {
+        get { Wendy.config._debug.get() }
+        set { Wendy.config._debug.set(newValue) }
+    }
+    
+    static var automaticallyRunTasks: Bool {
+        get { Wendy.config._automaticallyRunTasks.get() }
+        set { Wendy.config._automaticallyRunTasks.set(newValue) }
+    }
+    
+    class func addTaskRunnerListener(_ listener: TaskRunnerListener) {
+        Wendy.config._taskRunnerListeners.set { $0 + [WeakReferenceTaskRunnerListener(listener: listener)] }
+    }
+    
+    class func addTaskStatusListenerForTask(_ taskId: Double, listener: PendingTaskStatusListener) {
+        Wendy.config._taskStatusListeners.set { $0 + [TaskStatusListener(taskId: taskId, weakRefListener: WeakReferencePendingTaskStatusListener(listener: listener))] }
+
+        // The task runner could be running this task right now and because it takes a while potentially to run a task, I need to notify the listener here. This should be the only use case to handle here, running of a task.
+        if Wendy.shared.currentlyRunningTask?.taskId == taskId {
+            listener.running(taskId: taskId)
         }
-    }
-
-    class func logTaskSkipped(_ task: PendingTask, reason: ReasonPendingTaskSkipped) {
-        WendyConfig.taskRunnerListeners.forEach { weakRefListener in
-            weakRefListener.listener?.taskSkipped(task, reason: reason)
-        }
-        WendyConfig.getTaskStatusListenerForTask(task.taskId!).forEach { weakRefListener in
-            weakRefListener.listener?.skipped(taskId: task.taskId!, reason: reason)
-        }
-    }
-
-    class func logTaskComplete(_ task: PendingTask, successful: Bool, cancelled: Bool) {
-        let successful = (successful || cancelled) // to make sure that cancelled marks succcessful as successful, always.
-
-            WendyConfig.taskRunnerListeners.forEach { weakRefListener in
-                weakRefListener.listener?.taskComplete(task, successful: successful, cancelled: cancelled)
-            }
-            WendyConfig.getTaskStatusListenerForTask(task.taskId!).forEach { weakRefListener in
-                weakRefListener.listener?.complete(taskId: task.taskId!, successful: successful, cancelled: cancelled)
-            }
-    }
-
-    class func logTaskRunning(_ task: PendingTask) {
-            WendyConfig.taskRunnerListeners.forEach { weakRefListener in
-                weakRefListener.listener?.runningTask(task)
-            }
-            WendyConfig.getTaskStatusListenerForTask(task.taskId!).forEach { weakRefListener in
-                weakRefListener.listener?.running(taskId: task.taskId!)
-            }
-    }
-
-    class func logAllTasksComplete() {
-            WendyConfig.taskRunnerListeners.forEach { weakRefListener in
-                weakRefListener.listener?.allTasksComplete()
-            }
     }
 }
